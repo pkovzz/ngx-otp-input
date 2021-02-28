02af2b5c677f1ed62b1cf3f6080b9b9e63c94a2c
Peter <p.kovzz@gmail.com>
2021-02-28 17:18:12 +0100

    feat(component): basic stepping

--- projects/ngx-otp-input/src/lib/component/ngx-otp-input.component.ts
+++ projects/ngx-otp-input/src/lib/component/ngx-otp-input.component.ts
@@ -30,6 +30,7 @@ export class NgxOtpInputComponent implements OnInit, AfterViewInit {
   }
 
   private ngxOtpArray = new FormArray([]);
+  private focusedInputHasValue = false;
   private defaultAriaLabel = 'One time password input';
 
   constructor() {}
@@ -46,10 +47,26 @@ export class NgxOtpInputComponent implements OnInit, AfterViewInit {
     }
   }
 
-  getAriaLabelByIndex(currentIndex: number): string {
-    return this.ariaLabels[currentIndex]
-      ? this.ariaLabels[currentIndex]
+  getAriaLabelByIndex(index: number): string {
+    return this.ariaLabels[index]
+      ? this.ariaLabels[index]
       : this.defaultAriaLabel;
+  }
+
+  handleFocus(index: number): void {
+    this.getInputElementByIndex(index).select();
+  }
+
+  handleKeyup(value: string, index: number): void {
+    if (this.pattern.test(value)) {
+      this.stepForward(index);
+    } else if (value === 'Backspace') {
+      this.stepBackward(index);
+    }
+  }
+
+  handleKeydown(index: number): void {
+    this.focusedInputHasValue = !!this.getFormControlByIndex(index).value;
   }
 
   private setUpOtpForm(): void {
@@ -76,6 +93,20 @@ export class NgxOtpInputComponent implements OnInit, AfterViewInit {
     );
   }
 
+  private stepForward(index: number): void {
+    if (this.ngxOtpArray.valid) {
+      this.removeFocus(index);
+    } else if (index < this.config.otpLength - 1) {
+      this.setFocus(index + 1);
+    }
+  }
+
+  private stepBackward(index: number): void {
+    if (!this.focusedInputHasValue && index > 0) {
+      this.setFocus(index - 1);
+    }
+  }
+
   private setFocus(index: number): void {
     this.getInputElementByIndex(index).focus();
   }
@@ -84,7 +115,11 @@ export class NgxOtpInputComponent implements OnInit, AfterViewInit {
     this.getInputElementByIndex(index).blur();
   }
 
-  private getInputElementByIndex(index: number): HTMLElement {
+  private getInputElementByIndex(index: number): any {
     return this.otpInputElements.toArray()[index].nativeElement;
+  }
+
+  private getFormControlByIndex(index: number): FormControl {
+    return this.ngxOtpArray.controls[index] as FormControl;
   }
 }
