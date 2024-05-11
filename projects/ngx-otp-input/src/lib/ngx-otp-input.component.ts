7112e76ac96c0d817f322a79989fbb08f62df346
Péter Kovács <p.kov@icloud.com>
2024-05-11 11:15:17 +0200

    add eslint, prettier, husky

--- projects/ngx-otp-input/src/lib/ngx-otp-input.component.ts
+++ projects/ngx-otp-input/src/lib/ngx-otp-input.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-otp-input',
+  selector: 'ngx-otp-input',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      ngx-otp-input works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>ngx-otp-input works!</p> `,
+  styles: ``,
 })
-export class NgxOtpInputComponent {
-
-}
+export class NgxOtpInputComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxOtpInput } from 'ngx-otp-input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxOtpInput],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngx-otp-input-demo';
