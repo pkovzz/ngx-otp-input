eca72e20c740026bc6667e3b13bc5571c367703e
Péter Kovács <p.kov@icloud.com>
2024-06-01 17:12:09 +0200

    update everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxOtpInput } from 'ngx-otp-input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxOtpInput],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
