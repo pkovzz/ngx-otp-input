f82a5e790f8ed86b8c8a28051daa1a6cf4d7e308
Péter Kovács <p.kov@icloud.com>
2024-08-10 21:53:45 +0200

    simplify input handling

--- projects/ngx-otp-input/src/lib/directives/inputNavigations.directive.ts
+++ projects/ngx-otp-input/src/lib/directives/inputNavigations.directive.ts
@@ -17,40 +17,6 @@ export type OtpValueChangeEvent = [number, string];
   selector: '[ngxInputNavigations]',
 })
 export class InputNavigationsDirective implements AfterContentInit {
-  private readonly nonInputKeys = [
-    'Alt',
-    'Control',
-    'Meta',
-    'Shift',
-    'CapsLock',
-    'Backspace',
-    'Escape',
-    'ArrowLeft',
-    'ArrowUp',
-    'ArrowRight',
-    'ArrowDown',
-    'F1',
-    'F2',
-    'F3',
-    'F4',
-    'F5',
-    'F6',
-    'F7',
-    'F8',
-    'F9',
-    'F10',
-    'F11',
-    'F12',
-    'ContextMenu',
-    'Insert',
-    'Home',
-    'End',
-    'PageUp',
-    'PageDown',
-    'Tab',
-    'Enter',
-  ];
-
   private inputsArray: ElementRef<HTMLInputElement>[] = [];
 
   @ContentChildren('otpInputElement', { descendants: true })
@@ -103,35 +69,14 @@ export class InputNavigationsDirective implements AfterContentInit {
     }
   }
 
-  @HostListener('keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent): void {
-    if (
-      this.nonInputKeys.includes(event.key) ||
-      (event.code === 'KeyA' && event.ctrlKey === true) || // Allow: Ctrl+A
-      (event.code === 'KeyC' && event.ctrlKey === true) || // Allow: Ctrl+C
-      (event.code === 'KeyV' && event.ctrlKey === true) || // Allow: Ctrl+V
-      (event.code === 'KeyX' && event.ctrlKey === true) || // Allow: Ctrl+X
-      (event.code === 'KeyA' && event.metaKey === true) || // Cmd+A (Mac)
-      (event.code === 'KeyC' && event.metaKey === true) || // Cmd+C (Mac)
-      (event.code === 'KeyV' && event.metaKey === true) || // Cmd+V (Mac)
-      (event.code === 'KeyX' && event.metaKey === true) // Cmd+X (Mac)
-    ) {
-      return; // let it happen, don't do anything
-    } else if (!this.regexp.test(event.key)) {
-      event.preventDefault();
-    }
-  }
-
   @HostListener('input', ['$event'])
   onKeyUp(event: InputEvent): void {
     const index = this.findInputIndex(event.target as HTMLElement);
-    const inputValue = event.data;
-    if (
-      inputValue?.match(this.regexp) &&
-      !this.nonInputKeys.includes(inputValue)
-    ) {
-      this.valueChange.emit([index, inputValue]);
+    if (event.data?.match(this.regexp)) {
+      this.valueChange.emit([index, event.data]);
       this.setFocus(index + 1);
+    } else {
+      this.inputsArray[index].nativeElement.value = '';
     }
   }
 }
